f30bbda839174098214e963a3490dc42465c5079
ee <e2gamedev@outlook.com>
2024-02-19 19:21:36 +0100

    removed .git and package-lock

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env bun
 import chalk from 'chalk';
 import { execSync } from 'child_process';
+import fs from 'fs';
 import { join } from 'path';
 
 let projectName = process.argv[2];
@@ -10,10 +11,20 @@ const projectPath = join(process.cwd(), projectName);
 try {
     execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git ${projectPath}`);
     process.chdir(projectPath);
+
+    const gitPath = join(projectPath, '.git');
+    if (fs.existsSync(gitPath)) {
+        fs.rmdirSync(gitPath, { recursive: true });
+    }
+    const packageLockPath = join(projectPath, 'package-lock.json');
+    if (fs.existsSync(packageLockPath)) {
+        fs.unlinkSync(packageLockPath);
+    }
+
     execSync('bun i');
 }
 catch (err) {
-    console.log(chalk.red("Directory", projectPath, "already exists, or something else went wrong."));
+    console.log(chalk.red("Directory", projectPath, "already exists, or something else went wrong.", err));
     process.exit(1);
 }
 
