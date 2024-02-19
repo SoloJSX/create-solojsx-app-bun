1bbac250ab2b03582a954222bbcbb24f8a8bde9c
ee <e2gamedev@outlook.com>
2024-02-19 18:54:50 +0100

    fixed omit project name to clone to current dir

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ import chalk from 'chalk';
 import { execSync } from 'child_process';
 import { join } from 'path';
 
-const projectName = process.argv[2];
-if (!projectName) {
-    console.error('Please provide a project name');
-    process.exit(1);
-}
+let projectName = process.argv[2];
+projectName ??= ".";
+
 const projectPath = join(process.cwd(), projectName);
 try {
     execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git ${projectPath}`);
     process.chdir(projectPath);
-    execSync('npm i');
+    execSync('bun i');
 }
 catch (err) {
     console.log(chalk.red("Directory", projectPath, "already exists, or something else went wrong."));
@@ -20,6 +18,7 @@ catch (err) {
 }
 
 console.log(chalk.green("Project", chalk.green.bold(projectName), "created!"));
-console.log(chalk.blue("Now run the following commands:"));
-console.log(`cd ${projectName}`);
+console.log(chalk.blue("Now run the following command" + ((projectName != ".") ? "s" : "") + ":"));
+if (projectName != ".")
+    console.log(`cd ${projectName}`);
 console.log(`bun dev`);
