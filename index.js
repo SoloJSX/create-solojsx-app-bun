6939a8c588d8f7c556fbe7d35f88de4cdaeda692
ee <e2gamedev@outlook.com>
2024-02-21 02:35:46 +0100

    create in folder with existing .git folder now works

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env bun
 import chalk from 'chalk';
 import { execSync } from 'child_process';
-import fs from 'fs';
 import { join } from 'path';
 
 let projectName = process.argv[2];
@@ -9,18 +8,10 @@ projectName ??= ".";
 
 const projectPath = join(process.cwd(), projectName);
 try {
-    execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git ${projectPath}`);
+    execSync(`git clone https://github.com/eekelof/SoloJSX-template-ts.git`);
+    execSync(`rsync -av --progress SoloJSX-template-ts/ ${projectPath}/ --exclude '.git' --exclude 'package-lock.json'`);
+    execSync(`rm -rf SoloJSX-template-ts`);
     process.chdir(projectPath);
-
-    const gitPath = join(projectPath, '.git');
-    if (fs.existsSync(gitPath)) {
-        fs.rmdirSync(gitPath, { recursive: true });
-    }
-    const packageLockPath = join(projectPath, 'package-lock.json');
-    if (fs.existsSync(packageLockPath)) {
-        fs.unlinkSync(packageLockPath);
-    }
-
     execSync('bun i');
 }
 catch (err) {
