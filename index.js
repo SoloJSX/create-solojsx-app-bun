f440e543f59c56f169abd7fed5cf38d6cd891894
eekelof <u@w>
2024-08-26 02:53:04 +0200

    added support for js template

--- index.js
+++ index.js
@@ -3,23 +3,30 @@ import chalk from 'chalk';
 import { execSync } from 'child_process';
 import { join } from 'path';
 
+const getRepo = (a) => {
+    switch (a) {
+        case "-m":
+            return "SoloJSX-template-ts-minimal";
+        case "-js":
+            return "SoloJSX-template-js";
+    }
+    return "SoloJSX-template-ts";
+}
+
 const a2 = process.argv[2];
 const a3 = process.argv[3];
 
-let projectName = ".";
-if (a2 != "-m")
-    projectName = a2 || ".";
+const dirName = (!a2 || a2.startsWith("-")) ? "." : a2;
+const repoName = getRepo((dirName == ".") ? a2 : a3);
 
-let version = (a2 == "-m" || a3 == "-m") ? "-minimal" : "";
-const projectPath = join(process.cwd(), projectName);
+const projectPath = join(process.cwd(), dirName);
 try {
-    if (projectName != ".")
-        execSync(`mkdir ${projectName}`);
+    if (dirName != ".")
+        execSync(`mkdir ${dirName}`);
     process.chdir(projectPath);
 
-    const repoName = "SoloJSX-template-ts" + version;
     const repo = "https://github.com/SoloJSX/" + repoName + ".git";
-    execSync(`git clone ` + repo);
+    execSync(`git clone --quiet ` + repo);
     execSync(`rsync -av --progress ` + repoName + `/ ${projectPath}/ --exclude '.git' --exclude 'package-lock.json' --exclude 'LICENSE'`);
     execSync(`rm -rf ` + repoName);
     execSync('bun i');
@@ -28,6 +35,6 @@ catch (err) {
     console.log(chalk.red("Directory", projectPath, "already exists, or something else went wrong. \N Error:", err));
     process.exit(1);
 }
-console.log(chalk.green("Project", chalk.green.bold(projectName), "created!"));
-console.log(chalk.blue("Now run the following command" + ((projectName != ".") ? "s" : "") + ":"));
-console.log(((projectName != ".") ? (`cd ${projectName}\n`) : "") + `bun run dev`);
+console.log(chalk.green("Project", chalk.green.bold(dirName), "created!"));
+console.log(chalk.blue("Now run the following command" + ((dirName != ".") ? "s" : "") + ":"));
+console.log(((dirName != ".") ? (`cd ${dirName}\n`) : "") + `bun run dev`);
